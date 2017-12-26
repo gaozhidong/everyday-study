cf3250f166c51d11442394e0b737d17c5fb0165b
gaozhidong <1011284295@qq.com>
2017-12-26 18:26:12 +0800

    模块化

--- 038-day/MVC-demo/src/index.js
+++ 038-day/MVC-demo/src/index.js
@@ -1,30 +1,6 @@
 import './style.scss'
 import $ from  'jquery'
 
-let $module1 = $('.module1')
-let $module2 = $('.module2')
-let $module3 = $('.module3')
-
-$module1.on('change','input',function(){
-    console.log('change1')
-})
-
-$module2.on('change','input',function(){
-    console.log('change2')
-})
-
-$module3.on('change','input',function(){
-    console.log('change3')
-})
-
-$module1.on('click','button',function(){
-    console.log('click1')
-})
-
-$module2.on('click','button',function(){
-    console.log('click2')
-})
-
-$module3.on('click','button',function(){
-    console.log('click3')
-})
+import './module1'
+import './module2'
+import './module3'
