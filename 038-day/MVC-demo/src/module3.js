c95e5933ee9db837ef8c550508170aab64ed29ed
gaozhidong <1011284295@qq.com>
2017-12-26 19:37:06 +0800

    new

--- 038-day/MVC-demo/src/module3.js
+++ 038-day/MVC-demo/src/module3.js
@@ -1,12 +1,39 @@
 import $ from  'jquery'
 
-let $module3 = $('.module3')
+// let $module3 = $('.module3')
 
-$module3.on('change','input',function(){
-    console.log('change3')
-})
+// $module3.on('change','input',function(){
+//     console.log('change3')
+// })
+
+// $module3.on('click','button',function(){
+//     console.log('click3')
+// })
 
 
-$module3.on('click','button',function(){
-    console.log('click3')
+function  Maker(options){
+    this.$element = $(options.element)
+    this.events = options.events
+    this.bindEvents()
+}
+
+Maker.prototype.bindEvents = function(){
+    for(let key in this.events){
+        let parts = key.split(' ')
+        let eventType = parts.shift()
+        let selector = parts.join(' ')
+        this.$element.on(eventType,selector,this.events[key])
+    }
+}
+
+new Maker({
+    element:'.module3',
+    events:{
+        'change input':function(e){
+            console.log('change3')
+        },
+        'click button':function(e){
+            console.log('click3')
+        }
+    }
 })
