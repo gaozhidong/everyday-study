c847ab8853c09ed69c2a95b1d9e04a7736b77ca8
gaozhidong <1011284295@qq.com>
2017-12-27 14:52:19 +0800

    监听事件

--- 038-day/MVC-demo/src/module1.js
+++ 038-day/MVC-demo/src/module1.js
@@ -3,24 +3,27 @@ import Controller from './Controller'
 new Controller({
     element: '.module1',
     events: {
-        'change input': function (e) {
-            console.log('change1')
-        },
+        'change input': 'onChangeInput',
         'click button': 'addToCart'
     },
     addToCart: function () {
-        console.log(this)
         let value = this.$element.find('input').value
         this.remoteAddCart(value).then(() => {
             this.showAnimation()
         })
     },
     remoteAddCart: function () {
+        console.log('远程请求')
         //$.ajax....
         // return Promise
         return Promise.resolve(2)
     },
     showAnimation: function () {
+        console.log('展示动画')
         // this.$element.animate('...')
+    },
+    onChangeInput:function(e){
+        let input = e.currentTarget
+        console.log(input.value)
     }
 })
