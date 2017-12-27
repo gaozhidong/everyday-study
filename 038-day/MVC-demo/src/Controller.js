aff5d39f85a58c133567f67b0d1a027c533241f9
gaozhidong <1011284295@qq.com>
2017-12-27 14:46:06 +0800

    bind

--- 038-day/MVC-demo/src/Controller.js
+++ 038-day/MVC-demo/src/Controller.js
@@ -1,17 +1,32 @@
 import $ from 'jquery'
 
-class Controller{
-    constructor(options){
-       this.$element = $(options.element)
-        this.events = options.events
+class Controller {
+    constructor(options) {
+        for (let key in options) {
+            this[key] = options[key]
+        }
+        this.$element = $(options.element)
         this.bindEvents()
     }
-    bindEvents(){
-        for(let key in this.events){
+    bindEvents() {
+        for (let key in this.events) {
             let parts = key.split(' ')
             let eventType = parts.shift()
             let selector = parts.join(' ')
-            this.$element.on(eventType,selector,this.events[key])
+            if (typeof this.events[key] === 'function') {
+                this.$element.on(eventType, selector, this.events[key])
+            } else if (typeof this.events[key] === 'string') {
+                let methodName = this.events[key]
+                // let that = this
+                // this.$element.on(eventType, selector, function(e){
+                //     that[methodName].call(that,e)
+                // })
+
+                this.$element.on(eventType,selector,this[methodName].bind(this))
+            }
+
+
+
         }
     }
 }
--- 038-day/MVC-demo/src/module1.js
+++ 038-day/MVC-demo/src/module1.js
@@ -1,13 +1,26 @@
 import Controller from './Controller'
 
 new Controller({
-    element:'.module1',
-    events:{
-        'change input':function(e){
+    element: '.module1',
+    events: {
+        'change input': function (e) {
             console.log('change1')
         },
-        'click button':function(e){
-            console.log('click1')
-        }
+        'click button': 'addToCart'
+    },
+    addToCart: function () {
+        console.log(this)
+        let value = this.$element.find('input').value
+        this.remoteAddCart(value).then(() => {
+            this.showAnimation()
+        })
+    },
+    remoteAddCart: function () {
+        //$.ajax....
+        // return Promise
+        return Promise.resolve(2)
+    },
+    showAnimation: function () {
+        // this.$element.animate('...')
     }
 })
